c5abb345b80a5a84ddf8282dd43039fe785d0c55
nawabshaik2004-pr <nawabshaik2004@gmail.com>
2025-12-30 18:46:50 +0530

    commit

---
+++ src/conditionalstatements/ifstatement.java
@@ -0,0 +1,18 @@
+package conditionalstatements;
+
+public class ifstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        int num = 10;
+		        if (num > 0) {
+		            System.out.println(" Positive Number ");
+		        }
+		    }
+		
+
+
+	}
+
+
